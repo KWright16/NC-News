95b1d4c32743f7a24fd26f6029e492d5a7d77900
KWright16 <42691009+KWright16@users.noreply.github.com>
2018-11-30 12:42:00 +0000

    submitting

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,31 +3,37 @@ import { Link } from "@reach/router";
 
 const Sidebar = props => {
   return (
-    <div className="sidebar">
-      <section>
-        {props.user.username ? (
-          <div>
-            <p className="user">Logged in as {props.user.username}</p>
-            <Link className="link" to="/logout">
-              <p className="sidebar-text">Logout</p>
+    <>
+      <input className="menu-opener" type="checkbox" id="nav-toggle" />
+      <label className="menu-opener" for="nav-toggle">
+        Menu
+      </label>
+      <nav className="sidebar">
+        <section class="nav-list">
+          {props.user.username ? (
+            <div>
+              <p className="user">Logged in as {props.user.username}</p>
+              <Link className="link" to="/logout">
+                <p className="sidebar-text">Logout</p>
+              </Link>
+            </div>
+          ) : (
+            <Link className="link" to="/login">
+              <p className="sidebar-text">Login</p>
             </Link>
-          </div>
-        ) : (
-          <Link className="link" to="/login">
-            <p className="sidebar-text">Login</p>
-          </Link>
-        )}
+          )}
 
-        <br />
-        <Link className="link" to="/">
-          <p className="sidebar-text">Home</p>
-        </Link>
-        <br />
-        <Link className="link" to="/articles/new_article">
-          <p className="sidebar-text">Post New Article</p>
-        </Link>
-      </section>
-    </div>
+          <br />
+          <Link className="link" to="/">
+            <p className="sidebar-text">Home</p>
+          </Link>
+          <br />
+          <Link className="link" to="/articles/new_article">
+            <p className="sidebar-text">Post New Article</p>
+          </Link>
+        </section>
+      </nav>
+    </>
   );
 };
 
