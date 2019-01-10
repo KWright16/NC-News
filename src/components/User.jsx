5024dc904e1792e39254462f89d9851b1f1caf10
KWright16 <42691009+KWright16@users.noreply.github.com>
2019-01-10 21:27:13 +0000

    added user chart

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -2,16 +2,18 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import * as api from "../api";
 import { navigate } from "@reach/router";
+import { Doughnut } from 'react-chartjs-2'
 
 class User extends Component {
   state = {
     userArticles: [],
     isLoading: true,
     totalVotes: 0,
-    mostPopular: []
+    mostPopular: [],
+    data: {}
   };
   render() {
-    const { isLoading, userArticles, totalVotes, mostPopular } = this.state;
+    const { isLoading, userArticles, totalVotes, mostPopular, data } = this.state;
     const { user } = this.props;
 
     if (isLoading) return <div className="loader">Loading...</div>;
@@ -41,9 +43,13 @@ class User extends Component {
               );
             })
           ) : (
-            <li>You haven't written any articles yet</li>
-          )}
+              <li>You haven't written any articles yet</li>
+            )}
         </ul>
+        <div className='chart'>
+          <h3>What you've been writting about</h3>
+          <Doughnut data={data} id='chart' width={100} options={{ maintainAspectRatio: false, responsive: true }} />
+        </div>
       </div>
     );
   }
@@ -54,15 +60,36 @@ class User extends Component {
         const userArticles = articles.filter(
           article => article.created_by.username === this.props.user.username
         );
-        this.setState({ userArticles, isLoading: false });
+        const topicData = articles.reduce((acc, article) => {
+          acc[article.belongs_to] = (acc[article.belongs_to] || 0) + 1
+          return acc
+        }, {})
+        const data = {
+          labels: Object.keys(topicData),
+          datasets: [{
+            data: Object.values(topicData),
+            backgroundColor: [
+              '#eeaa7b',
+              '#07889b',
+              '#66b9bf'
+            ],
+            hoverBackgroundColor: [
+              '#e37222',
+              '#e37222',
+              '#e37222'
+            ]
+          }]
+        }
+        this.setState({ userArticles, isLoading: false, data });
       })
       .catch(err => {
         const { uri } = this.props;
+        console.log(err)
         navigate("/error", {
           replace: true,
           state: {
-            code: err.response.status,
-            message: err.response.statusText,
+            // code: err.response.status,
+            // message: err.response.statusText,
             from: uri
           }
         });
